11030b26afd48eec2fea1c11e4f17e7d4b676acb
diogogoncalves <dmgdark95@gmail.com>
2026-02-06 00:17:02 +0000

    Base Project

---
+++ Modules/Holdings/database/seeders/HoldingsDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Holdings\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class HoldingsDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Holdings/resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<x-holdings::layouts.master>
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('holdings.name') !!}</p>
+</x-holdings::layouts.master>
